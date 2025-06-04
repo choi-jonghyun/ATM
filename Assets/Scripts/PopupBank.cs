1dc200bdbf2ad144c2264ca34e077440fdcd99fd
choi-jonghyun <choiwhj1@naver.com>
2025-06-04 17:17:01 +0900

    feat : popup
    -popup끄기 켜기

---
+++ Assets/Scripts/PopupBank.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupBank : MonoBehaviour
+{
+    public GameObject depositPanel;
+    public GameObject withdrawalPanel;
+    public GameObject dW;
+
+    private void Start()
+    {
+        off();
+    }
+
+    public void ShowDeposit()
+    {
+        depositPanel.SetActive(true);
+        withdrawalPanel.SetActive(false);
+        dW.SetActive(false);
+        
+
+    }
+
+    public void ShowWithdraw()
+    {
+        depositPanel.SetActive(false);
+        withdrawalPanel.SetActive(true);
+        dW.SetActive(false);
+    }
+
+    public void off()
+    {
+        depositPanel.SetActive(false);
+        withdrawalPanel.SetActive(false);
+        dW.SetActive(true) ;
+    }
+}
