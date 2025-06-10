a1bab235bee2783bbc735d90f66e23213c7903aa
choi-jonghyun <choiwhj1@naver.com>
2025-06-10 14:23:32 +0900

    fix : userdata
    userdata 수정

--- Assets/Scripts/UserData.cs
+++ Assets/Scripts/UserData.cs
@@ -4,14 +4,20 @@ using System;
 [System.Serializable]
 public class UserData
 {
+    public string UserID;
+    public string Password;
+
     public string UserName;
     public int Cash;
     public int Balance;
 
     
 
-    public UserData(string userName, int cash, int balance)
+    public UserData(string userID, string password,string userName, int cash, int balance)
     {
+        UserID = userID;
+        Password = password;
+
         UserName = userName;
         Cash = cash;
         Balance = balance;
